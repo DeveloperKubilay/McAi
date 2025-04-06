a3829ce5ed9f71a522fc25fcd451422b48d183ef
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-04-06 05:51:58 +0300

    first commit

---
+++ modules/noloadreq/dbsetup.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+    db.set("players",[{
+        "name": "valancess",
+        "work": "The King",
+        "itsabot":false
+    },{
+        "name": botname,
+        "work": "farmer",
+        "itsabot":true
+    }])
+
+
+}
