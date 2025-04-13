62a94b3299c543b336908a0eaa2a32be3ab3e88e
DeveloperKubilay <kubiss52@yahoo.com>
2025-04-14 00:32:12 +0300

    Add initial project setup with dependencies, world data, and bot functionality
    - Created package.json with necessary dependencies for the project.
    - Added test.js to parse and log messages from a JSON structure.
    - Introduced worlddata.json to store player and world information.
    - Implemented y.js to create a bot that looks at the nearest entity in the game.

---
+++ modules/noloadreq/globalg.js
@@ -0,0 +1,20 @@
+module.exports = {
+  getinv: function() {
+    var inventoryArray = [];
+    bot.inventory.items().forEach((item) => {
+      inventoryArray.push(`${item.name}x${item.count}`);
+    });
+    return inventoryArray;
+  },
+  getmyeq: function() {
+    const equipment = {
+      mainHand: bot.heldItem ? bot.heldItem.name : 'empty',
+      offHand: bot.inventory.slots[45] ? bot.inventory.slots[45].name : 'empty',
+      head: bot.inventory.helmet ? bot.inventory.helmet.name : 'empty',
+      chest: bot.inventory.chestplate ? bot.inventory.chestplate.name : 'empty',
+      legs: bot.inventory.leggings ? bot.inventory.leggings.name : 'empty',
+      feet: bot.inventory.boots ? bot.inventory.boots.name : 'empty'
+    };
+    return equipment;
+  },
+}
